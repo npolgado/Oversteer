1fd55d661b1bdd0f7efdd82f6b58a55de6049795
nicholas <nicholas831@gmail.com>
2026-03-27 21:35:15 -0700

    fix testing hook

--- .githooks/pre-push.cmd
+++ .githooks/pre-push.cmd
@@ -1,6 +1,9 @@
 @echo off
 echo Running old test suite...
-node --test test/*.test.js
+setlocal enabledelayedexpansion
+set FILES=
+for %%f in (test\*.test.js) do set FILES=!FILES! %%f
+node --test %FILES%
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 echo Running new test suite...
 npx vitest run
